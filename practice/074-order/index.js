4ba134969ddfdf227367514def06d1b394789feb
unknown <smsxjenn@126.com>
2018-06-20 21:55:55 +0800

    order-619

--- practice/074-order/index.js
+++ practice/074-order/index.js
@@ -1,3 +1,96 @@
+const AdminPage = {
+    data() {
+        return {
+            error: [],
+            current: {},
+            list: [],
+            show_form: false,
+            keyword: '',
+            timer: null,
+        };
+    },
+    mounted() {
+        this.read();
+    },
+    methods: {
+        search(e) {
+            if (e)
+                e.preventDefault();
+
+            let keyword = this.keyword;
+
+            let param = { or: { name: keyword } };
+            http.post(`${this.model}/search`, param)
+                .then(r => {
+                    this.list = r.data.data;
+                });
+        },
+        create(e) {
+            e.preventDefault();
+
+            if (!this.validate())
+                return;
+
+            let is_update = this.current.id;
+            let action = is_udate ? 'update' : 'create';
+
+            http
+                .post(`${this.model}/${action}`, this.current)
+                .then(r => {
+                    if (r.data.success) {
+                        this.current = {};
+                        if (!is_update)
+                            this.list.push(r.data.data);
+                    }
+                });
+        },
+        remove(id) {
+            if (!confirm('确定要删除吗'))
+                return;
+
+            http
+                .post(`${this.model}/delete`, { id })
+                .then(r => {
+                    if (r.data.success) {
+                        util.delete_element_by_id(this.list, id);
+                    }
+                });
+        },
+        read() {
+            http
+                .post(`${this.model}/read`)
+                .then(r => {
+                    this.list = r.data.data;
+                });
+        },
+        validate(row) {
+            row = row || this.current;
+            this.error = [];
+
+            this.validate_props.forEach(prop => {
+
+                let r = this[`validate` + prop] ();
+
+                if (r === true)
+                return;
+
+                this.error.push(r);
+            });
+
+            return !this.error.length;
+        },
+    },
+    watch : {
+        keyword() {
+            clearTimeout(this.timer);
+
+            this.timer = setTimeout(() => {
+                this.search();
+            },300);
+        },
+    },
+};
+
 const Home = Vue.component('home', {
     template: `<div>
     <h1>欢迎来到背背山吃饭大学</h1>
@@ -27,6 +120,9 @@ const AdminTable = Vue.component('admin-table', {
     template: `<div>
     <h2>桌号管理</h2>
     <form @submit="create($event)">
+      <div v-if="error.length" class="error">
+        <div v-for="e in error">{{e}}</div>
+      </div>
       <div class="input-wrap">
         <label>桌号</label>
         <input type="text" v-model="current.name">
@@ -45,19 +141,27 @@ const AdminTable = Vue.component('admin-table', {
       <tr>
       <th>桌号</th>
       <th>座位数</th>
+      <th>操作</th>
       </tr>
       </thead>
       <tbody>
         <tr v-for="row in list">
         <td>{{row.name}}</td>
         <td>{{row.capacity}}</td>
+        <td>
+          <button @click="current = row">更新</button>
+          <button @click="remove(row.id)">删除</button>
+        </td>
         </tr>
       </tbody>
     </table>
-    </div>`,
+  </div>
+    `,
 
     data() {
         return {
+            validate_props: ['name', 'capacity'],
+            error: [],
             current: {},
             list: [],
         };
@@ -65,16 +169,79 @@ const AdminTable = Vue.component('admin-table', {
     methods: {
         create(e) {
             e.preventDefault();
+
+            if (!this.validate())
+                return;
+            let is_update = this.current.id;
+            let action = is_update ? 'update' : 'create';
+
             http
-                .post('table/create', this.current)
+                .post(`table/${action}`, this.current)
                 .then(r => {
                     if (r.data.success) {
                         this.current = {};
-                        this.list.push(r.data.data);
+                        if (!is_update)
+                            this.list.push(r.data.data);
                     }
                 });
         },
+        remove(id) {
+            if (!confirm('确定要删除吗？'))
+                return;
+
+            http
+                .post('table/delete', { id })
+                .then(r => {
+                    if (r.data.success) {
+                        util.delete_element_by_id(this.list, id);
+                    }
+                });
+        },
+
+        validate(row) {
+            row = row || this.current;
+
+            this.error = [];
+
+            this.validate_props.forEach(prop => {
+                let r = this['validate_' + prop]();
+                if (r === true)
+                    return true;
+
+                this.error.push(r);
+            });
+
+            return !this.error.length;
+        },
+
+        validate_name(value) {
+            value = value || this.current.name;
+            const MAX_LENGTH = 255;
+            if (!value)
+                return '桌号为必填项';
+
+            if (value.length >= MAX_LENGTH)
+                return '此项最大长度为${MAX_LENGT}';
+
+            return true;
+        },
+
+        validate_capacity(value) {
+            value = value || this.current.capacity;
+
+            if (!value)
+                return '座位数为必填项';
+
+            if (value < 1 || value > 100000)
+                return '不合法的座位数';
+
+            return true;
+        },
+
     },
+
+
+    //初始化 
     mounted() {
         http.post('table/read')
             .then(r => {
---
+++ practice/074-order/util.js
@@ -0,0 +1,13 @@
+window.util = {
+    find_index_by_id(arr,id) {
+        return arr.findIndex(row => {
+            return row.id == id;
+        });
+    },
+
+    delete_element_by_id(arr,id) {
+        let i = this.find_index_by_id(arr,id);
+        arr.splice(i,1);
+    },
+};
+
